ae764f9313fd9108aa6ecb9b0b7ef2f0659667b6
Jean Lúcio <jeanlucio@gmail.com>
2026-05-14 11:12:32 +0000

    add tests/generator/lib.php for PHPUnit data generator

---
+++ tests/generator/lib.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Data generator for mod_playerwords.
+ *
+ * @package    mod_playerwords
+ * @category   test
+ * @copyright  2026 Jean Lúcio
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Data generator class for the playerwords activity module.
+ */
+class mod_playerwords_generator extends testing_module_generator {
+    /**
+     * Creates a new instance of the playerwords activity.
+     *
+     * @param array|\stdClass|null $record Field values for the instance.
+     * @param array|null $options Module options (e.g. idnumber, section).
+     * @return \stdClass Created course-module record.
+     */
+    public function create_instance($record = null, array $options = null): \stdClass {
+        $record = (object)(array)$record;
+
+        $defaults = [
+            'sources'            => 1,
+            'glossaryid'         => 0,
+            'min_length'         => 4,
+            'max_length'         => 6,
+            'max_attempts'       => 6,
+            'timer_seconds'      => 0,
+            'show_ranking'       => 1,
+            'wordmode'           => 1,
+            'max_rounds'         => 0,
+            'cooldown_seconds'   => 86400,
+            'completionattempts' => 0,
+            'grade'              => 100,
+            'gradepass'          => 0.0,
+            'grademethod'        => 1,
+            'hud_round_cost_item' => 0,
+            'hud_round_cost_qty'  => 1,
+            'hud_hint_cost_item'  => 0,
+            'hud_hint_cost_qty'   => 1,
+        ];
+
+        foreach ($defaults as $field => $value) {
+            if (!isset($record->$field)) {
+                $record->$field = $value;
+            }
+        }
+
+        return parent::create_instance($record, $options);
+    }
+}
