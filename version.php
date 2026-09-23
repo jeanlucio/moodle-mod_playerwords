7c543f521a3cf911ebc744343529de5ffd7b3a2f
Jean Lúcio <jeanlucio@gmail.com>
2026-09-23 22:02:48 +0000

    declare Moodle 5.3 support
    The two new CI legs (main branch) are green: PHPUnit, Behat and the
    full lint suite, alongside the existing 4.5-5.2 legs.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_playerwords';
-$plugin->version   = 2026090800;
+$plugin->version   = 2026092300;
 $plugin->requires  = 2024100700; // Moodle 4.5+.
-$plugin->supported = [405, 502];
+$plugin->supported = [405, 503];
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'v1.0.5';
